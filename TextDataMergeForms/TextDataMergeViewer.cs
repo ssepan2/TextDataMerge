b59e542b38b24a602de6535f149b649b339390f7
Stephen Sepan <sjsepan@outlook.com>
2020-12-03 13:36:19 -0500

    fix null viewmodel reference in status message assignment on file exit

--- TextDataMergeForms/TextDataMergeViewer.cs
+++ TextDataMergeForms/TextDataMergeViewer.cs
@@ -121,7 +121,7 @@ namespace TextDataMergeForms
                 else if (e.PropertyName == "StatusMessage")
                 {
                     //replace default action by setting control property
-                    StatusBarStatusMessage.Text = ViewModel.StatusMessage;
+                    StatusBarStatusMessage.Text = (ViewModel != null ? ViewModel.StatusMessage : (String)null);
                     //e = new PropertyChangedEventArgs(e.PropertyName + ".handled");
 
                     //ConsoleApplication.defaultOutputDelegate(String.Format("{0}", StatusMessage));
@@ -129,7 +129,7 @@ namespace TextDataMergeForms
                 else if (e.PropertyName == "ErrorMessage")
                 {
                     //replace default action by setting control property
-                    StatusBarErrorMessage.Text = ViewModel.ErrorMessage;
+                    StatusBarErrorMessage.Text = (ViewModel != null ? ViewModel.ErrorMessage : (String)null);
                     //e = new PropertyChangedEventArgs(e.PropertyName + ".handled");
 
                     //ConsoleApplication.defaultOutputDelegate(String.Format("{0}", ErrorMessage));
